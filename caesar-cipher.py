7b7842247027c58d39bd7aeec904e2a5d1209f80
Alex Slyusarev <aslyusarev@gmail.com>
2022-11-13 16:46:37 -0800

    added decrypt function

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,4 +1,4 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
@@ -13,4 +13,12 @@ def encrypt(input_text, input_shift):
   encrypted_word = "".join(encrypted_list)
   print(encrypted_word)
 
-encrypt(text, shift)
+def decrypt(input_text, input_shift):
+  decrypted_list = []
+  for letter in input_text:
+    index = alphabet.index(letter)
+    decrypted_list.append(alphabet[index - input_shift])
+
+  decrypted_word = "".join(decrypted_list)
+  print(decrypted_word)
+
