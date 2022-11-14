71b754469203f316b4af7bab9a880bd69a49f7f6
Alex Slyusarev <aslyusarev@gmail.com>
2022-11-13 17:33:06 -0800

    refactored code into a single function

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,24 +1,38 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+print(logo)
+direction = input("\nType 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(input_text, input_shift):
-  encrypted_list = []
-  for letter in input_text:
+def cipher(text, shift, direction):
+  word = ""
+  for letter in text:
     index = alphabet.index(letter)
-    encrypted_list.append(alphabet[index + input_shift])
+    if direction == "encode":
+        word += alphabet[index + shift]
+    elif direction == "decode":
+      word += alphabet[index - shift]
+    else: 
+      print("Invalid entry")
+  print(f"Your word is {word}")
 
-  encrypted_word = "".join(encrypted_list)
-  print(encrypted_word)
-
-def decrypt(input_text, input_shift):
-  decrypted_list = []
-  for letter in input_text:
-    index = alphabet.index(letter)
-    decrypted_list.append(alphabet[index - input_shift])
-
-  decrypted_word = "".join(decrypted_list)
-  print(decrypted_word)
-
+cipher(text, shift, direction)
